79388cd9e72d70c0399c36c46906a8ca6c23d28b
Jon <jon.aronoff@utah.edu>
2026-03-03 09:42:18 -0700

    testing

--- src/lammpsParser/__init__.py
+++ src/lammpsParser/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from ._core import __doc__, __version__,parse_frame_from_file
+from .lammpsParser import __doc__, __version__,parse_frame_from_file
 
 __all__ = ["__doc__", "__version__","parse_frame_from_file"]
 
